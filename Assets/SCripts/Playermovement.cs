74eac8a286d6cdf56dc49f99ca17d5f4c6ccfcaf
TheStone9 <nstone83@proton.me>
2024-05-29 16:12:49 -0400

    Re-added WASD movement

---
+++ Assets/SCripts/Playermovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+    }
+}
